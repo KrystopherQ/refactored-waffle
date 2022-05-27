8e290de4afba7a8f2516fdf89a0394355dcc29f4
Krystopher Quintero <krystopher.q@gmail.com>
2022-05-26 21:03:34 -0700

    add logic to a method that accepts content and adds to db and add logic to method that gets all content from db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,22 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) =>{
+  const todoDB = await openDB('jate', 1)
+  const tx = todoDB.transaction('jate', 'readwrite')
+  const store = tx.objectStore('jate')
+  const req = store.put({ id: 1, content: content })
+  const res = await req
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () =>{
+  const todoDB = await openDB('jate', 1)
+  const tx = todoDB.transaction('jate', 'readonly')
+  const store = tx.objectStore('jate')
+  const req = store.getAll()
+  const res = await req
+  return res.content
+}
 
 initdb();
